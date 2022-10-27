18d11790ea2587bda2da9b48c84f36fe404512fa
itochs <halglobe0108@gmail.com>
2022-10-27 23:12:46 +0900

    add run bot setting

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import discord
 from mcserverbot import MCServerBot
 from dotenv import load_dotenv
 load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+server_bot = mcserverbot.MCServerBot(command_prefix='!', intents=intents)
+server_bot.run(os.getenv("TOKEN"))

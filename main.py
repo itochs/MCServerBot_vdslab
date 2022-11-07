2fb71d88c62bee30fa22e751873679b110f43518
itochs <halglobe0108@gmail.com>
2022-11-07 18:57:00 +0900

    fix type miss

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import discord
-from mcserverbot import MCServerBot
+from MCServerBot import MCServerBot
 from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-server_bot = mcserverbot.MCServerBot(command_prefix='!', intents=intents)
+server_bot = MCServerBot(command_prefix='!', intents=intents)
 server_bot.run(os.getenv("TOKEN"))
